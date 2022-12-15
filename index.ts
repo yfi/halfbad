353cce03ad129e9b484fde5a8a24f2c5a491497c
Yarek Waszul <yarek@yarekwaszul.com>
2022-12-15 13:34:47 -0500

    halfbad

--- index.ts
+++ index.ts
@@ -1,15 +1,10 @@
 export function range(start: number, end: number, inc: number = 1): number[] {
-
   let range = []
   inc = Math.abs(inc)
 
-  if (start < end)
-    for (let i = start; i < end; i += inc) range.push(i)
+  if (start < end) for (let i = start; i < end; i += inc) range.push(i)
 
-  
-  if(start >= end)
-    for (let i = start; i > end; i -= inc) range.push(i)
+  if (start >= end) for (let i = start; i > end; i -= inc) range.push(i)
 
   return range
-
 }
