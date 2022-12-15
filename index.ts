a98a7f81043b2a0bf5549e07357c59f4c9174fb7
Yarek Waszul <yarek@yarekwaszul.com>
2022-12-15 12:32:40 -0500

    range fn

--- index.ts
+++ index.ts
@@ -1,27 +1,15 @@
-export function range(start:number,end:number,inc:number=1):array{
-  
-  let range = [];
+export function range(start: number, end: number, inc: number = 1): number[] {
+
+  let range = []
   inc = Math.abs(inc)
-  console.log(inc)
+
+  if (start < end)
+    for (let i = start; i < end; i += inc) range.push(i)
+
   
-if (start<end) {
-  
-   for (let i = start; i < end ; i+=inc) range.push(i)
-   
-   
-} else {
-  
-   for (let i = start; i > end; i-=inc)  range.push(i)
-   
-   
-}
+  if(start >= end)
+    for (let i = start; i > end; i -= inc) range.push(i)
 
   return range
-  
-}
 
-const r = range(0,5)
-const rr = range(4,1)
-const rrr = range(0,10,2)
-const rrrr = range(5,5)
-console.log(r,rr,rrr,rrrr)
+}
